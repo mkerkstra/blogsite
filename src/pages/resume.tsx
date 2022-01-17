a858aa1995815f5f32d6ad86248e16529c789742
Matt Kerkstra <mattkerkstra@gmail.com>
2022-01-16 18:51:21 -0600

    add placeholder resume and blog

---
+++ src/pages/resume.tsx
@@ -0,0 +1,69 @@
+/* eslint-disable max-len */
+import React from 'react';
+
+/**
+ *
+ * sections:
+ *  where I've worked
+ *    - projects
+ *    - awards
+ *  libraries I love to work with
+ *  what I'd like to learn next?
+ *  people I like to work with + links?
+ *
+ */
+
+export default function Resume() {
+  return (
+    <div className='h-100% overflow-auto'>
+      <h1>Lorem Ipsum</h1>
+      <body>
+        <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque sodales sapien imperdiet magna pretium iaculis. Nam ac ultrices ex. Etiam condimentum vestibulum nisi. Morbi elementum sapien ipsum, quis lobortis ante accumsan vitae. Nullam volutpat cursus laoreet. Curabitur mauris mauris, venenatis in rhoncus vitae, interdum scelerisque lacus. Proin ut quam in tortor porttitor rhoncus. In eu libero varius, euismod ligula ut, laoreet dolor. Suspendisse ultrices purus non magna rutrum gravida. Nam nibh enim, aliquet sed maximus et, scelerisque et lorem. Pellentesque feugiat, velit id venenatis laoreet, arcu dolor rhoncus tortor, ut gravida turpis massa id turpis. Maecenas vehicula erat ut ligula porta laoreet. Vestibulum faucibus mauris eros, sit amet imperdiet tortor aliquet sit amet. Suspendisse non turpis aliquet, placerat lacus sed, sodales magna. In efficitur tincidunt lacinia.
+        </p>
+
+        <p>
+        Sed eu magna porttitor, mattis sem ac, ornare tortor. Maecenas et justo et urna pharetra tincidunt a quis libero. Vestibulum aliquet, tortor id feugiat vulputate, mi risus luctus nisl, non vestibulum sem purus in nulla. Cras lacinia luctus magna quis molestie. Donec pellentesque ornare turpis, id aliquam mi pulvinar non. Nullam sollicitudin consectetur erat eu egestas. Aenean sed sapien lectus. Sed faucibus et sem vitae malesuada. Cras et libero non metus vehicula lobortis. Ut rhoncus urna eu tincidunt efficitur. Fusce consequat orci arcu, sed fermentum nulla fringilla non.
+        </p>
+        <p>
+        Interdum et malesuada fames ac ante ipsum primis in faucibus. Proin sed imperdiet mauris. Suspendisse pulvinar lectus libero, sit amet maximus nulla venenatis eget. Quisque mattis elit at lorem laoreet imperdiet. Morbi blandit eu felis non venenatis. Cras dictum vitae tortor vel auctor. Nunc laoreet ipsum sed diam luctus molestie. Quisque leo nisi, congue at sagittis et, eleifend vitae odio. Proin nisl tellus, fermentum et elementum porta, vehicula eu purus. Phasellus sit amet ligula id purus vestibulum tempor. Pellentesque vehicula imperdiet risus, ac pellentesque metus dapibus non.
+        </p>
+        <p>
+        Cras ac iaculis dui. Vivamus eros libero, semper sit amet tellus in, posuere porttitor ex. Mauris bibendum ornare lectus, nec efficitur lorem venenatis rutrum. Morbi pellentesque finibus ligula, a dignissim erat convallis a. Quisque pretium malesuada lorem, in vestibulum enim tincidunt vitae. Aliquam tempus, dolor quis sollicitudin aliquam, leo nibh blandit leo, imperdiet molestie urna neque ultrices purus. Vivamus mollis suscipit odio. Quisque malesuada at est sed dignissim. Etiam eros leo, condimentum non maximus eu, pulvinar at justo. Fusce nulla ipsum, blandit eget erat non, congue pellentesque libero. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Morbi blandit odio tellus, vel vehicula metus hendrerit quis. Suspendisse scelerisque iaculis dui, et posuere odio blandit suscipit. Integer nec volutpat justo, eget luctus libero. Fusce non magna accumsan leo convallis faucibus et id elit.
+        </p>
+        <p>
+        Vestibulum eleifend, turpis eu consequat venenatis, dolor ante dictum nisl, vel vehicula orci lorem vitae tortor. Pellentesque mauris nisi, dignissim et est sollicitudin, ornare euismod ligula. Proin et dolor vel erat consectetur sollicitudin. Nullam ex augue, sagittis et vulputate et, semper et tortor. Donec ut placerat lectus, et malesuada leo. Curabitur in venenatis turpis, et semper risus. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        </p>
+      </body>
+      {/* <h1>Resume</h1>
+      <section>
+        <h2>Where I&aposve Worked</h2>
+        <ul>
+          <li>
+            <a href='https://www.reyrey.com/company'
+              target='_blank'
+              rel='noopener noreferrer'
+            >Reynolds and Reynolds</a>
+            <span>02/2019 - 06-2020</span>
+          </li>
+          <li>
+            <a href='https://www.hotelengine.com/about-us/'
+              target='_blank'
+              rel='noopener noreferrer'
+            >Hotel Engine</a>
+            <span>07-2020-Present</span>
+          </li>
+        </ul>
+      </section>
+      <section>
+        <h2>Tools I Love to Work With</h2>
+      </section>
+      <section>
+        <h2>What I&aposd Like to Learn Next</h2>
+      </section>
+      <section>
+        <h2>People I Like to Work With</h2>
+      </section> */}
+    </div>
+  );
+}
