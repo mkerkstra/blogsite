fa0976af6a935a3f2474c3c4b1b79867efad7dea
Matt Kerkstra <mattkerkstra@gmail.com>
2022-01-27 03:15:24 -0600

    opacity/color in current route in navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,37 +3,44 @@ import DarkModeSwitch from './DarkModeSwitch';
 import AriaLink from '../components/ariaLink';
 
 const classes = {
-  nav: `relative w-screen h-[10%] py-5 flex items-center place-content-around
- border-b 
-  dark:bg-[#FFFFFF] bg-[#2C2C40] 
-  dark:text-black text-white`,
+  nav: `
+  relative w-screen h-[10%] py-5 flex items-center place-content-around
+  border-b  
+  dark:bg-black bg-white
+  dark:text-white text-black`,
   links: `
     my-auto
   `,
 };
+
+const routes = {
+  home: '/',
+  blog: '/blog',
+  resume: '/resume',
+} as const;
 
 const Navbar = () => {
   const router = useRouter();
   return (
     <nav className={classes.nav}>
       <AriaLink
-        disabled={router.pathname === '/'}
-        href='/'
-        className={classes.links}
+        disabled={router.pathname === routes.home}
+        href={routes.home}
+        className={`${classes.links} ${router.pathname === routes.home && 'text-gray-700 opacity-50'}`}
       >
         home
       </AriaLink>
       <AriaLink
-        disabled={router.pathname === '/blog'}
-        href='/blog'
-        className={classes.links}
+        disabled={router.pathname === routes.blog}
+        href={routes.blog}
+        className={`${classes.links} ${router.pathname === routes.blog && 'text-gray-700 opacity-50'}`}
       >
         blog
       </AriaLink>
       <AriaLink
-        disabled={router.pathname === '/resume'}
-        href='/resume'
-        className={classes.links}
+        disabled={router.pathname === routes.resume}
+        href={routes.resume}
+        className={`${classes.links} ${router.pathname === routes.resume && 'text-gray-700'}`}
       >
         resume
       </AriaLink>
