949528ba903d66a5de8b3bd58c2496c0f6be2fff
Matt Kerkstra <mattkerkstra@gmail.com>
2022-02-03 23:06:45 -0600

    fix dark/light mode icons

--- src/components/DarkModeSwitch.tsx
+++ src/components/DarkModeSwitch.tsx
@@ -28,82 +28,61 @@ const darkModeIcon = () => (
         group-hover:translate-y-[1.25%] group-hover:-scale-0
         group:hover:opacity-0 delay-250 duration-500 ease-in-out
       `}/>
-      <div className={`
-        my-auto
-        mr-[8%]
-        center
-        h-[95%]
-        w-[95%]
-        bg-black
-        rounded-full
-        transition-transform group-hover:translate-x-[2.5%]
-        group-hover:translate-y-[1.25%] group-hover:-scale-0
-        group:hover:opacity-100 delay-250 duration-500 ease-in-out
-      `}/>
     </div>
   </div>
 );
 
+const lightModeStyles = {
+  button: `
+    relative h-full w-full
+    hover:animate-[spin_2s_linear_infinite]
+  `,
+  rayContainer: `
+      container
+      absolute
+      bottom-[40.5%]
+  `,
+  rays: `
+    origin-center 
+    border-black
+  `,
+  sun: `
+    absolute
+    border-2
+    w-[60%]
+    h-[60%]
+    left-[20%]
+    top-[20%]
+    border-black
+    bg-white
+    rounded-full
+  `,
+};
+
 const lightModeIcon = () => (
-  <div className={`
-    grid
-    grid-rows-[100%]
-    grid-cols-[100%]
-    group
-    h-full
-    w-full
-    bg-white;
-  `}>
-    <div className={`
-      justify-self-center
-      h-full
-      w-full
-      rotate-180
-      border-t-2 border-black
-    `}/>
-    <div className={`
-      justify-self-center
-      h-full
-      w-full
-      border-t-2 border-black
-      rotate-45
-    `}/>
-    <div className={`
-      justify-self-center
-      h-full
-      w-full
-      border-t-2 border-black
-      rotate-225
-    `}/>
-    <div className={`
-      justify-self-center
-      right-1/2
-      h-full
-      w-full
-      border-t-2 border-black
-      rotate-90
-    `}/>
-    <div className={`
-      justify-self-center
-      background-color:white
-      h-[60%]
-      w-[60%]
-      rounded-full
-    `}/>
+  <div className={lightModeStyles.button}>
+    <div className={lightModeStyles.rayContainer}>
+      <hr className={lightModeStyles.rays}/>
+      <hr className={`
+        ${lightModeStyles.rays}
+        rotate-90`
+      }/>
+      <hr className={`
+        ${lightModeStyles.rays}
+        rotate-45
+        -translate-y-[2.5px]
+        -translate-x-[.5px]
+      `}/>
+      <hr className={`
+        ${lightModeStyles.rays}
+        -rotate-45
+        -translate-y-[2.5px]
+        -translate-x-[.5px]
+      `}/>
+    </div>
+    <div className={lightModeStyles.sun}/>
   </div>
 );
-// const switchContainer = `
-//   dark:bg-white dark:border-[#494949]
-//   bg-black border-white
-//   focus:outline-none focus-visible:ring-2
-//   focus-visible:ring-[#a8a9ff8c]
-//   focus-visible:ring-opacity-75
-//   relative inline-flex flex-shrink-0
-//   h-[1.68rem] w-[2.95rem]
-//   border-2
-//   rounded-full cursor-pointer
-//   transition-colors ease-in-out duration-200
-// `;
 
 export default function DarkModeSwitch() {
   const [darkMode, switchDarkMode] = useDarkMode();
@@ -116,15 +95,6 @@ export default function DarkModeSwitch() {
       {
         darkMode ? darkModeIcon() : lightModeIcon()
       }
-      {/* <span
-        className={`
-            dark:translate-x-0 dark:bg-[#494949]
-            translate-x-[1.25rem] bg-white
-            pointer-events-none inline-block
-            h-[1.5rem] w-[1.5rem] rounded-full
-            shadow-lg
-            transform ring-0 transition ease-in-out duration-200`}/> */}
     </Switch>
   );
 }
-
